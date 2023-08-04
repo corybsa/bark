da8846392c0d40d4750a15374c6ded219751f857
Cory Sandlin <corybsa@gmail.com>
2023-08-04 15:42:33 -0400

    fix version message

--- cuda_test.py
+++ cuda_test.py
@@ -1,6 +1,6 @@
 import torch
 
-print(f'PyTorch version: {torch.version}')
+print(f'PyTorch version: {torch.__version__}')
 
 if torch.cuda.is_available():
 	print(f'CUDA is available, version is:{torch.version.cuda}')
