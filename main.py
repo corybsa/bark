febff34b8d09a7ca74081ece87ac6c9dac616226
Cory Sandlin <corybsa@gmail.com>
2023-08-05 09:41:13 -0400

    nifty enhancements

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import sys
 import os
 from pathlib import Path
+from colorama import init as colorama_init, Fore, Style
 
 
 class VoiceGenerator:
@@ -27,6 +28,8 @@ class VoiceGenerator:
           self.generate_audio_menu = {
                'Generate audio': self.prompt_for_text_to_speech,
                'Save current voice model': self.save_voice_model,
+               'Adjust text generation temperature': self.prompt_for_text_temp,
+               'Adjust waveform generation temperature': self.prompt_for_waveform_temp,
                'Back': lambda : self.present_menu(self.main_menu)
           }
 
@@ -38,26 +41,31 @@ class VoiceGenerator:
           os.system('cls' if os.name=='nt' else 'clear')
      
 
+     def print(self, message, color=Fore.WHITE):
+          print(f'{color}{message}{Style.RESET_ALL}')
+
+
      def present_menu(self, menu_items):
-          print(f'\n(using voice model \'{self.current_voice_model_name}\')')
-          print('Choose an option to continue:')
+          self.print(f'\n(using voice model \'{self.current_voice_model_name}\')', Fore.CYAN)
+          self.print(f'(text generation temperature: {self.text_temp}, waveform generation temperature: {self.waveform_temp})', Fore.CYAN)
+          self.print('Choose an option to continue:')
 
           # display menu items
           items = list(menu_items.items())
           for i in range(len(items)):
-               print(str(i + 1) + ': ' + items[i][0])
+               self.print(str(i + 1) + ': ' + items[i][0])
 
           # get user input and show error if they did not enter a number
           try:
                choice = int(input('Enter your choice: '))
           except:
-               print(f'\nInvalid choice. Please enter a number between 1 and {len(menu_items.items())}.\n')
+               self.print(f'\nInvalid choice. Please enter a number between 1 and {len(menu_items.items())}.', Fore.RED)
                self.present_menu(menu_items)
                return
 
           # show error if they entered a number that is not in the menu
           if choice < 1 or choice > len(menu_items):
-               print(f'\nInvalid choice. Please enter a number between 1 and {len(menu_items.items())}.\n')
+               self.print(f'\nInvalid choice. Please enter a number between 1 and {len(menu_items.items())}.', Fore.RED)
                self.present_menu(menu_items)
                return
 
@@ -67,6 +75,8 @@ class VoiceGenerator:
 
      def save_voice_model(self):
           if self.is_using_built_in_model:
+               self.generate_voice_model('generating new model')
+
                model_name = input('Enter a name for the model: ')
                model_name += '.npz'
                
@@ -75,11 +85,11 @@ class VoiceGenerator:
                try:
                     save_as_prompt(filepath, self.current_voice_model)
                except:
-                    print('\nPlease generate at least one audio sample before saving the voice model.')
+                    self.print('\nPlease generate at least one audio sample before saving the voice model.', Fore.YELLOW)
                     self.present_menu(self.generate_audio_menu)
                     return
 
-               print('\nVoice model saved to ' + filepath + '.')
+               self.print(f'\nVoice model saved to {filepath}.', Fore.GREEN)
                
                with np.load(filepath) as data:
                     self.current_voice_model_name = model_name.replace('.npz', '')
@@ -89,12 +99,12 @@ class VoiceGenerator:
                          'fine_prompt': data['fine_prompt']
                     }
                
-               print(f'Changed current voice model to {model_name.replace(".npz", "")}.')
+               self.print(f'Changed current voice model to {model_name.replace(".npz", "")}.', Fore.CYAN)
                self.is_using_built_in_model = False
           else:
-               filepath = os.path.join(self.voice_models_dir, model_name)
+               filepath = os.path.join(self.voice_models_dir, self.current_voice_model_name + '.npz')
                save_as_prompt(filepath, self.current_voice_model)
-               print('Voice model saved to ' + filepath + '.\n')
+               self.print(f'\nVoice model saved to {filepath}.', Fore.GREEN)
 
 
      def load_voice_model(self):
@@ -102,19 +112,19 @@ class VoiceGenerator:
           voices = [f for f in os.listdir(self.voice_models_dir) if os.path.isfile(os.path.join(self.voice_models_dir, f)) and f.endswith('.npz')]
           
           if len(voices) == 0:
-               print('\nNo voice models found in ' + self.voice_models_dir + '.\n')
+               self.print(f'\nNo voice models found in {self.voice_models_dir}.\n', Fore.RED)
                self.present_menu(self.main_menu)
           else:
                print('\nSelect a voice model to load:')
                for i in range(len(voices)):
-                    print(str(i + 1) + ': ' + voices[i])
+                    self.print(str(i + 1) + ': ' + voices[i])
                
-               print(str(len(voices) + 1) + ': Back')
+               self.print(str(len(voices) + 1) + ': Back')
 
                try:
                     voice_choice = int(input('Enter your choice: '))
                except:
-                    print(f'\nInvalid choice. Please enter a number between 1 and {len(voices)}.')
+                    self.print(f'\nInvalid choice. Please enter a number between 1 and {len(voices)}.', Fore.RED)
                     self.load_voice_model()
                     return
                
@@ -124,7 +134,7 @@ class VoiceGenerator:
                     return
                # show error if they entered a number that is not in the menu
                elif voice_choice < 1 or voice_choice > len(voices):
-                    print(f'\nInvalid choice. Please enter a number between 1 and {len(voices)}.')
+                    self.print(f'\nInvalid choice. Please enter a number between 1 and {len(voices)}.', Fore.RED)
                     self.load_voice_model()
                     return
                     
@@ -135,13 +145,14 @@ class VoiceGenerator:
                          'coarse_prompt': data['coarse_prompt'],
                          'fine_prompt': data['fine_prompt']
                     }
+                    self.is_using_built_in_model = False
                
                self.present_menu(self.generate_audio_menu)
      
 
      def continue_with_current_voice_model(self):
           if self.current_voice_model is None:
-               print('\nNo voice model loaded.')
+               self.print('\nNo voice model loaded.', Fore.RED)
                self.present_menu(self.main_menu)
                return
           else:
@@ -149,16 +160,16 @@ class VoiceGenerator:
 
 
      def prompt_for_built_in_voice_models(self):
-          print('\nEnter a voice preset or leave blank for default voice (default is v2/en_speaker_2)')
+          self.print('\nA list of all the built-in preset voice models:')
 
           sorted_prompts = sorted(generation.ALLOWED_PROMPTS)
 
           for i in range(len(sorted_prompts)):
-               print(str(i + 1) + ': ' + sorted_prompts[i])
+               self.print(str(i + 1) + ': ' + sorted_prompts[i])
           
-          print(str(len(sorted_prompts) + 1) + ': Back')
+          self.print(str(len(sorted_prompts) + 1) + ': Back')
 
-          choice = input('Enter your choice: ')
+          choice = input('Enter your choice or leave blank for default (default is v2/en_speaker_2): ')
 
           # user selected default
           if choice == '':
@@ -171,7 +182,7 @@ class VoiceGenerator:
           try:
                choice_int = int(choice)
           except:
-               print(f'\nInvalid choice. Please enter a number between 1 and {len(sorted_prompts)}.')
+               self.print(f'\nInvalid choice. Please enter a number between 1 and {len(sorted_prompts)}.', Fore.RED)
                self.prompt_for_built_in_voice_models()
                return
           
@@ -181,7 +192,7 @@ class VoiceGenerator:
                return
           # show error if they entered a number that is not in the menu
           elif choice_int < 1 or choice_int > len(sorted_prompts):
-               print(f'\nInvalid choice. Please enter a number between 1 and {len(sorted_prompts)}.')
+               self.print(f'\nInvalid choice. Please enter a number between 1 and {len(sorted_prompts)}.', Fore.RED)
                self.prompt_for_built_in_voice_models()
                return
           
@@ -198,12 +209,13 @@ class VoiceGenerator:
                self.present_menu(self.main_menu)
                return
 
-          print('\nEnter text to convert to speech')
-          print('Notes:')
-          print('  - To indicate the end of input, press ctrl+z then press enter (must be on a blank line)')
-          print('  - To start over press ctrl+r at any point. Then indicate the end of input by pressing ctrl+z on a blank line.')
-          print('  - To cancel and return to the main menu press ctrl+x at any point. Then indicate the end of input by pressing ctrl+z on a blank line.')
-          print('---------')
+          self.print('\nNotes:')
+          self.print('  - To suggest that the voice sing, add a music note (♪) to the start and end of the line.')
+          self.print('  - To indicate the end of input, press ctrl+z then press enter (must be on a blank line).')
+          self.print('  - To start over press ctrl+r at any point. Then indicate the end of input by pressing ctrl+z on a blank line.')
+          self.print('  - To cancel and return to the main menu press ctrl+x at any point. Then indicate the end of input by pressing ctrl+z on a blank line.')
+          self.print('\nEnter text to convert to speech')
+          self.print('---------')
 
           text_prompt = ''.join(sys.stdin.readlines()).replace('\n', ' ')
 
@@ -214,29 +226,12 @@ class VoiceGenerator:
           elif '\x18' in text_prompt or text_prompt == '':
                self.present_menu(self.generate_audio_menu)
                return
-          
-          filename = self.prompt_for_filename()
-          print('Generating voice model and audio file...')
-
-          try:
-               (model, audio) = generate_audio(
-                    text_prompt,
-                    history_prompt=self.current_voice_model,
-                    text_temp=self.text_temp,
-                    waveform_temp=self.waveform_temp,
-                    output_full=True
-               )
-               self.current_voice_model = model
-
-               # save audio to disk
-               write_wav(os.path.join(self.generated_output_dir, filename), SAMPLE_RATE, audio)
-               print('File written to \'' + self.generated_output_dir + '/' + filename + '\'\n\n')
-          except ValueError as e:
-               print('Error: ' + str(e) + '\n')
-               self.current_voice_model = None
-               self.current_voice_model_name = None
+          elif text_prompt == '':
+               self.print('\nPlease enter some text.', Fore.RED)
+               self.prompt_for_text_to_speech()
                return
           
+          self.generate_voice_model(text_prompt, save_audio=True)
           self.present_menu(self.generate_audio_menu)
      
 
@@ -251,10 +246,79 @@ class VoiceGenerator:
           return filename + '.wav'
 
 
+     def prompt_for_text_temp(self):
+          text_temp = input('\nEnter a text temperature (or ctrl+x to cancel): ')
+
+          # ctrl+x
+          if text_temp == '\x18':
+               self.present_menu(self.main_menu)
+               return
+
+          try:
+               text_temp = float(text_temp)
+          except:
+               self.print('\nInvalid temperature. Please enter a number between 0 and 1.', Fore.RED)
+               self.prompt_for_text_temp()
+               return
+          
+          self.text_temp = text_temp
+          self.present_menu(self.generate_audio_menu)
+
+
+     def prompt_for_waveform_temp(self):
+          waveform_temp = input('\nEnter a waveform temperature (or ctrl+x to cancel): ')
+
+          # ctrl+x
+          if waveform_temp == '\x18':
+               self.present_menu(self.main_menu)
+               return
+
+          try:
+               waveform_temp = float(waveform_temp)
+          except:
+               self.print('\nInvalid temperature. Please enter a number between 0 and 1.', Fore.RED)
+               self.prompt_for_waveform_temp()
+               return
+          
+          self.waveform_temp = waveform_temp
+          self.present_menu(self.generate_audio_menu)
+
+
+     def generate_voice_model(self, text_prompt, save_audio=False):
+          if save_audio:
+               filename = self.prompt_for_filename()
+               self.print('\nGenerating voice model and audio file...', Fore.CYAN)
+          else:
+               self.print('\nGenerating voice model...', Fore.CYAN)
+
+          try:
+               (model, audio) = generate_audio(
+                    text_prompt,
+                    history_prompt=self.current_voice_model,
+                    text_temp=self.text_temp,
+                    waveform_temp=self.waveform_temp,
+                    output_full=True
+               )
+               self.current_voice_model = model
+
+               # save audio to disk
+               if save_audio:
+                    filepath = os.path.join(self.generated_output_dir, filename)
+                    write_wav(filepath, SAMPLE_RATE, audio)
+                    self.print(f'\nFile written to \'{filepath}\'', Fore.GREEN)
+               else:
+                    self.print('\nVoice model generated', Fore.GREEN)
+          except ValueError as e:
+               self.print('Error: ' + str(e) + '\n', Fore.RED)
+               self.current_voice_model = None
+               self.current_voice_model_name = None
+               return
+
+
 def main():
      generator = VoiceGenerator()
      generator.cls()
-     print('Loading voice models...')
+     print(f'{Fore.CYAN}Loading voice models...{Style.RESET_ALL}')
 
      # download and load all models
      preload_models()
@@ -266,4 +330,5 @@ def main():
 
 
 if __name__ == "__main__":
+     colorama_init()
      main()
