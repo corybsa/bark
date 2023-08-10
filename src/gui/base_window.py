27c0c333be4cffa563f9e0489ab6581c767882c6
Cory Sandlin <corybsa@gmail.com>
2023-08-10 08:35:52 -0400

    create base class for windows

---
+++ src/gui/base_window.py
@@ -0,0 +1,42 @@
+from typing import Union
+import dearpygui.dearpygui as dpg
+
+
+class BaseWindow:
+  tag = 'base_window'
+
+  def show(self):
+    dpg.configure_item(self.tag, show=True)
+
+
+  def open_modal(self, message: str, tag: str, no_close = True, height: Union[int, str] = 'auto', width: Union[int, str] = 'auto'):
+    width_padding = 7
+
+    if height == 'auto':
+      height = (len(message.split('\n')) * 8)
+      height = 98 if height < 98 else height
+
+    if width == 'auto':
+      longest_message = max(message.split('\n'), key=len)
+      width = (len(longest_message) * 7) + width_padding * 2
+    
+    y = (dpg.get_viewport_height() / 2) - (height / 2) - 20
+    x = (dpg.get_viewport_width() / 2) - (width / 2) - width_padding
+
+    with dpg.window(
+      label=' ',
+      tag=tag,
+      show=True,
+      modal=True,
+      no_close=no_close,
+      no_collapse=True,
+      no_resize=True,
+      no_move=True,
+      no_title_bar=no_close,
+      pos=[x, y],
+      width=width,
+      height=height,
+      on_close=lambda: dpg.delete_item(tag)
+    ):
+      dpg.add_text(message)
+
