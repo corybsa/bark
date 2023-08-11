5387aa9a4a86d130fb69e64b338bf73894eb63f2
Cory Sandlin <corybsa@gmail.com>
2023-08-11 09:43:57 -0400

    finishing touches on save window

--- src/gui/save_window.py
+++ src/gui/save_window.py
@@ -1,5 +1,6 @@
 import dearpygui.dearpygui as dpg
 from wgbark import VoiceGenerator
+import os
 from .base_window import BaseWindow
 
 
@@ -7,16 +8,17 @@ class SaveWindow(BaseWindow):
   def __init__(self, generator: VoiceGenerator):
     self.generator = generator
     self.tag = 'save_window'
-    self.save_file_dialog = 'save_file_dialog'
+    self.save_file_dialog_tag = self.get_random_tag()
 
     with dpg.window(label='Save', tag=self.tag, show=False, pos=[20, 180]):
+      self.create_save_file_dialog()
       self.create_buttons()
   
 
   def create_buttons(self):
     dpg.add_button(
       label='Save current voice model',
-      callback=lambda: dpg.show_item(self.save_file_dialog)
+      callback=lambda: dpg.show_item(self.save_file_dialog_tag)
     )
     
     dpg.add_button(
@@ -27,18 +29,20 @@ class SaveWindow(BaseWindow):
 
   def create_save_file_dialog(self):
     with dpg.file_dialog(
-      tag=self.save_file_dialog,
+      tag=self.save_file_dialog_tag,
       directory_selector=False,
-      show=True,
+      show=False,
+      default_filename='',
       width=600,
       height=400,
       file_count=-1,
       default_path=self.generator.get_user_voice_models_dir(),
-      callback=lambda id, value: print(value['file_path_name'])
+      callback=lambda id, value: self.save_file(value['file_path_name'])
     ):
       dpg.add_file_extension('.npz')
   
 
-  def save_file(self, filename: str):
+  def save_file(self, filepath: str):
+    filename = os.path.basename(filepath).split('.')[0]
     self.generator.save_voice_model(filename)
 
